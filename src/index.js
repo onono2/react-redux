00e90abaacbbcd7267f624a0135d4b832474fd14
O-THINKPAD\suriy <suriyakamon@lannasoftworks.com>
2019-09-08 11:37:27 +0700

    Add isFetchingUser variable

--- src/index.js
+++ src/index.js
@@ -106,19 +106,19 @@ const pingReducer = (state = { isPinging: false, userinfo: "" }, action) => {
   }
 };
 
-// const isFetchingUser = (state = false, action) => {
-//   switch (action.type) {
-//     case "FETCH_USER":
-//       return true;
+const isFetchingUser = (state = false, action) => {
+  switch (action.type) {
+    case "FETCH_USER":
+      return true;
 
-//     case "FETCH_USER_FULFILLED":
-//     case "FETCH_USER_CANCELLED":
-//       return false;
+    case "FETCH_USER_FULFILLED":
+    case "FETCH_USER_CANCELLED":
+      return false;
 
-//     default:
-//       return state;
-//   }
-// };
+    default:
+      return state;
+  }
+};
 
 /*const store = createStore(
   combineReducers({
